3cf939c4cface5c1525eb503031dac994963a97a
JangHanjun <68003176+JangHanjun@users.noreply.github.com>
2020-07-14 13:36:05 +0900

    Update home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -14,6 +14,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String deleteId = ''; // showAlertDialog에 삭제 기능에 사용하기 위해 만듬
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +28,7 @@ class _MyHomePageState extends State<MyHomePage> {
               alignment: Alignment. centerLeft,
             ),
           ),
-          Expanded(child: memoBuilder()),
+          Expanded(child: memoBuilder(context)),
         ],
       ),
       floatingActionButton: FloatingActionButton.extended(
@@ -64,8 +65,46 @@ class _MyHomePageState extends State<MyHomePage> {
     return await sd.memos();
   }
 
+  Future<void> deleteMemo(String id) async {
+    DBHelper sd = DBHelper();
+    return await sd.deleteMemo(id);
+  }
 
-  Widget memoBuilder() {
+  void showAlertDialog(BuildContext context) async {
+    // async 라서 deletedId 초기화를 여기서 해야함.
+    String result = await showDialog(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('삭제 경고'),
+          content: Text("정말로 삭제하시겠습니까?\n삭제시 복구는 불가능합니다"),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('삭제'),
+              onPressed: () {
+                Navigator.pop(context, "삭제");
+                setState(() {
+                  deleteMemo(deleteId);
+                  deleteId = ''; //deleteId 다시 초기화
+                });
+              },
+            ),
+            FlatButton(
+              child: Text('취소'),
+              onPressed: () {
+                Navigator.pop(context, "취소");
+                deleteId = ''; //deleteId 다시 초기화
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+
+  Widget memoBuilder(BuildContext parentContext) {
     return FutureBuilder(
       builder: (context, Snap) {
         if (Snap.data.isEmpty) { //데이터가 없으면 아래 컨테이너 출력
@@ -78,66 +117,78 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           );
         }
+
+
         return ListView.builder(
           physics: BouncingScrollPhysics(), //스크롤시 쫀득하게
           itemCount: Snap.data.length,
           itemBuilder: (context, index) {
             Memo memo = Snap.data[index];
 
-            return Container(
-              margin: EdgeInsets.all(7),
-              padding: EdgeInsets.all(15),
-              height: 100,
-              alignment: Alignment.center,
+            return InkWell(
+              onTap: (){},
+              onLongPress: (){
+                setState(() {
+                  deleteId = memo.id;
+                  showAlertDialog(parentContext);
+                });
+              },  //길게 누를시의 기능
+              // onDoubleTap: (){},  //두번 누를 때
+              child: Container(
+                margin: EdgeInsets.all(7),
+                padding: EdgeInsets.all(15),
+                height: 100,
+                alignment: Alignment.center,
 
 
-              decoration: BoxDecoration(
-                color: const Color(000000000),
-                border: Border.all(
-                  color: Colors.black,
-                  width: 1,
-                ),
-                // boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 3)],
-                borderRadius: BorderRadius.circular(12),
-              ),
-
-
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: <Widget>[
-                      Text(
-                        memo.title,
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.w500
-                        ),
-                      ),
-                      Text(
-                        memo.text,
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-                    ],
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(230, 230, 230, 1), //RGB, Opacity
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 1,
                   ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: <Widget>[
-                      Text(
-                        "최종 수정 시간: " + memo.editTime.split('.')[0], //초 단위 까지만 나오게 수정
-                        style: TextStyle(
-                            fontSize: 12
+                  // boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 3)],
+                  borderRadius: BorderRadius.circular(12),
+                ),
+
+
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Text(
+                          memo.title,
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w500
+                          ),
                         ),
-                        textAlign: TextAlign.end, // 시간은 오른쪽 끝으로 보냄
-                      ),
-                    ],
-                  )
-                ],
+                        Text(
+                          memo.text,
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Text(
+                          "최종 수정 시간: " + memo.editTime.split('.')[0], //초 단위 까지만 나오게 수정
+                          style: TextStyle(
+                              fontSize: 12
+                          ),
+                          textAlign: TextAlign.end, // 시간은 오른쪽 끝으로 보냄
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
             );
           },
